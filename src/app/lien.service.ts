68f7d9b9b7e4d17a845d279ec6fe71cc55510fc5
youssouphSagna <youssouphsagna54@gmail.com>
2022-07-18 19:50:20 +0200

    fix module 7 and add module 8 to 13

--- src/app/lien.service.ts
+++ src/app/lien.service.ts
@@ -12,6 +12,12 @@ import * as module4Data from './data/module4.json';
 import * as module5Data from './data/module5.json';
 import * as module6Data from './data/module6.json';
 import * as module7Data from './data/module7.json';
+import * as module8Data from './data/module8.json';
+import * as module9Data from './data/module9.json';
+import * as module10Data from './data/module10.json';
+import * as module11Data from './data/module11.json';
+import * as module12Data from './data/module12.json';
+import * as module13Data from './data/module13.json';
 
 @Injectable()
 export class LienService {
@@ -24,6 +30,12 @@ export class LienService {
   module_05: Lien[] = [];
   module_06: Lien[] = [];
   module_07: Lien[] = [];
+  module_08: Lien[] = [];
+  module_09: Lien[] = [];
+  module_10: Lien[] = [];
+  module_11: Lien[] = [];
+  module_12: Lien[] = [];
+  module_13: Lien[] = [];
 
   constructor() {
     this.importModule0();
@@ -33,7 +45,13 @@ export class LienService {
     this.importModule4();
     this.importModule5();
     this.importModule6();
-    this.importModule6();
+    this.importModule7();
+    this.importModule8();
+    this.importModule9();
+    this.importModule10();
+    this.importModule11();
+    this.importModule12();
+    this.importModule13();
 
     this.liens = [
       ...this.module_00,
@@ -44,6 +62,12 @@ export class LienService {
       ...this.module_05,
       ...this.module_06,
       ...this.module_07,
+      ...this.module_08,
+      ...this.module_09,
+      ...this.module_10,
+      ...this.module_11,
+      ...this.module_12,
+      ...this.module_13,
     ];
   }
 
@@ -85,6 +109,31 @@ export class LienService {
   importModule7() {
     this.module_07 = (module7Data as any).default as Lien[];
     this.module_07.forEach((lien) => (lien.category = Category.MODULE_7));
+  }
+  importModule8() {
+    this.module_08 = (module8Data as any).default as Lien[];
+    this.module_08.forEach((lien) => (lien.category = Category.MODULE_8));
+  }
+  importModule9() {
+    this.module_09 = (module9Data as any).default as Lien[];
+    this.module_09.forEach((lien) => (lien.category = Category.MODULE_9));
+  }
+  importModule10() {
+    this.module_10 = (module10Data as any).default as Lien[];
+    this.module_10.forEach((lien) => (lien.category = Category.MODULE_10));
+  }
+  importModule11() {
+    this.module_11 = (module11Data as any).default as Lien[];
+    this.module_11.forEach((lien) => (lien.category = Category.MODULE_11));
+  }
+  importModule12() {
+    this.module_12 = (module7Data as any).default as Lien[];
+    this.module_12.forEach((lien) => (lien.category = Category.MODULE_12));
+  }
+
+  importModule13() {
+    this.module_13 = (module13Data as any).default as Lien[];
+    this.module_13.forEach((lien) => (lien.category = Category.MODULE_13));
   }
 
   get list() {
